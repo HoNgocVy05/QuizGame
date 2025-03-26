3c2a7853c01cb030a073f3fc5ad5b2a99c09d0a5
HỒ NGUYỄN QUỲNH HƯƠNG <quynhhuong397999@gmail.com>
2025-03-26 22:39:27 +0700

    Cập nhật logic trong Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Kích hoạt phục vụ file tĩnh
+app.UseStaticFiles();
+app.UseDefaultFiles();
 
+// Chạy ứng dụng
 app.Run();
